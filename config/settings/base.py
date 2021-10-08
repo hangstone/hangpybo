b9f83eca08695af524e7159a26361dfb6f4419b6
hangstone <hangstonee@gmail.com>
2021-10-08 15:52:36 +0900

    copy static files for admin

--- config/settings/base.py
+++ config/settings/base.py
@@ -123,9 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = []
 
 # 로그인 성공 시, 자동으로 이동할 URL
 LOGIN_REDIRECT_URL = '/'
